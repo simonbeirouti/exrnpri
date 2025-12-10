c03d61310500b96863151c727eab41d4a6515c32
simonbeirouti <hello@simonbeirouti.com>
2025-12-11 02:00:18 +1100

    bugfixing for contract

--- entrypoint.js
+++ entrypoint.js
@@ -1,10 +1,13 @@
-// entrypoint.js
-
-// Import required polyfills first
-// IMPORTANT: These polyfills must be installed in this order
 import "react-native-get-random-values";
 import "@ethersproject/shims";
 import { Buffer } from "buffer";
 global.Buffer = Buffer;
+
+Buffer.prototype.subarray = function subarray(begin, end) {
+    const result = Uint8Array.prototype.subarray.apply(this, [begin, end]);
+    Object.setPrototypeOf(result, Buffer.prototype);
+    return result;
+};
+
 // Then import the expo router
 import "expo-router/entry";
